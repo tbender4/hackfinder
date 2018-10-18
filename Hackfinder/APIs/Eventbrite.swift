e81bf045f778f59493de62cdcbe317c987ab65df
Thomas Bender <tbender4@gmail.com>
2018-10-18 18:01:38 -0400

    updates .gitignore. adds basic Eventbrite file

---
+++ Hackfinder/APIs/Eventbrite.swift
@@ -0,0 +1,14 @@
+//
+//  Eventbrite.swift
+//  Hackfinder
+//
+//  Created by Thomas Bender on 10/18/18.
+//  Copyright © 2018 Thomas Bender. All rights reserved.
+//
+
+import Foundation
+
+class Eventbrite {
+  static let baseURL = ""
+  static let oAuth2Token  = ""     //This is kept in a separate file for security purposes. The file is listed under .gitignore and thus will NOT be pushed to the public repository.
+}
