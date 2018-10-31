5b0651aa9b8c0bd6b9dc359f196fb09007dfd59e
kairi <kairi@kairis-MacBook-Pro.local>
2018-10-30 20:19:13 -0400

    Detail MainStory board feature

---
+++ Hackfinder/View Controllers/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Hackfinder
+//
+//  Created by kairi on 10/30/18.
+//  Copyright © 2018 Thomas Bender. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var LogoImageView: UIImageView!
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
